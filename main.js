0252a623185f5e60d2562825434763a899cc1206
Oscar Ko <OscarKoEmail@gmail.com>
2023-02-06 12:40:23 -0500

    Add template code

---
+++ main.js
@@ -0,0 +1,17 @@
+/* CONSTANTS AND GLOBALS */
+const width = window.innerWidth * .8;
+const height = 600;
+const margin = 100;
+
+/* LOAD DATA */
+
+console.log("helloooo")
+// d3.csv('./data.csv', d3.autoType)
+//   .then(data => {
+
+//     console.log(data)
+
+//   });
+
+
+
